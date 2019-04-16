41a7fdf4cb8f885bee14961c913f8441c88adcc7
MatiasEKrenz <matiaskrenz@gmail.com>
2019-04-16 14:48:16 -0300

    Prueba de API mercadolibre

---
+++ src/main/java/Sites.java
@@ -0,0 +1,25 @@
+public class Sites {
+
+    private String id;
+    private String name;
+
+    public Sites() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,16 +6,51 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 public class Test {
 
     public static void main(String[] args) {
 
+        Sites[] sites = null;
+
         try {
-            String data = readUrl("https://api.mercadolibre.com/sites/MLA/categories");
+            String sitesData = readUrl("https://api.mercadolibre.com/sites");
+            sites = new Gson().fromJson(sitesData, Sites[].class);
+
+            for (Sites site : sites){
+                System.out.println(site.getName());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Ocurrio un error al traer las categorias.");
+            e.printStackTrace();
+        }
+
+        boolean bandera = true;
+        String id = "";
+
+        while (bandera){
+
+            System.out.println("\nSeleccione un sitio: ");
+
+            Scanner scanner = new Scanner(System.in);
+            String categoria = scanner.nextLine();
+
+            for (Sites site : sites){
+                if (site.getName().equals(categoria)){
+                    bandera = false;
+                    id = site.getId();
+                }
+            }
+
+        }
+
+        try {
+            String data = readUrl("https://api.mercadolibre.com/sites/" + id + "/categories");
             Category[] categories = new Gson().fromJson(data, Category[].class);
             for (Category category : categories){
-                System.out.println(category.getName());
+                System.out.println("\n" + category.getName());
             }
         } catch (IOException e) {
             System.out.println("Ocurrio un error al traer las categorias.");
